3fb13feb6a7b17086db5e00825645cbdb99e4ad0
IsmoilObidov <84452090+IsmoilObidov@users.noreply.github.com>
2022-06-18 20:28:40 +0500

    '

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,50 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
-
 <body>
-    <form id="whois-lookup-form" action="" method="POST">
-        {{ method_field('POST') }}
-        {{ csrf_field() }}
-            <label for="domain">Domain</label>
-            <input name="domain" type="text" class="form-control"
-                placeholder="Domain name">
-            @if ($errors->has('domain'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('domain') }}</strong>
-                </span>
-            @endif
-        </div>
+	<form id="whois-lookup-form" action="" method="POST">
+		{{ method_field('POST') }}
+		{{ csrf_field() }}
+		<div class="form-group{{ $errors->has('domain') ? ' has-error' : '' }}">
 
-        <button type="submit" class="btn btn-default btn-block btn-lg">Search</button>
-    </form>
+			<label for="domain">Domain</label>
+			<input name="domain" type="text" class="form-control" placeholder="Domain name">
 
-    <br>
+			@if ($errors->has('domain'))
+				<span class="help-block">
+					<strong>{{ $errors->first('domain') }}</strong>
+				</span>
+			@endif
+			
+		</div>
+	
+		<button type="submit" class="btn btn-default btn-block btn-lg">Search</button>
+	</form>
+	
+	<br>
+	
+	<div id="response"></div>
+	
+	
+	
+	
 
-    <div id="response"></div>
+		
+</body>
+</html>
 
 
-
-
-    <script>
-        $(function() {
-            $("#whois-lookup-form").on("submit", function(event) {
-                event.preventDefault();
-                var request = $.ajax({
-                    url: $(this).attr('action'),
-                    method: $(this).attr('method'),
-                    data: $(this).serialize(),
-                    dataType: "json",
-                    cache: false
-                });
-            });
-        });
-
-        </body> 
-		</html>
